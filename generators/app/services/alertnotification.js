a250b529c032a4cbed9fb9656d1bf935b71d7016
Chris Vignola <cvignola@us.ibm.com>
2017-10-01 22:24:42 -0500

    idt scripts and readme

--- generators/app/services/alertnotification.js
+++ generators/app/services/alertnotification.js
@@ -1,3 +1,14 @@
 'use strict';
-let alertnotification = {}
+let alertnotification = [
+  {
+    "url": "alert_url",
+    "username": "alert-username",
+    "password": "alert-password",
+    "serviceInfo": {
+      "label": "alert-label",
+      "name": "alert-name",
+      "plan": "alert-plan"
+    }
+  }
+]
 exports.alertnotification = alertnotification;
